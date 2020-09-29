cac303f19df6de22bf768176202b37ff2615e865
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-29 18:07:56 +0500

    001-variables.js file

--- javascript/book/001-variables.js
+++ javascript/book/001-variables.js
@@ -1,1 +1,20 @@
-var name = "Abubakar";
+// objective: Variables
+// 1. Much Like Algebra
+// 2. JavaScript Identifiers
+// 3. The Assignment Operator
+// 4. JavaScript Data Types
+// 5. Declaring (Creating) JavaScript Variables
+// 6. One Statement, Many Variables
+// 7. Value = undefined
+// 8. Re-Declaring JavaScript Variables
+// 9. JavaScript Arithmetic
+// 10. JavaScript Dollar Sign $
+// 11. JavaScript Underscore (_)
+
+var a = "Abubakar";
+var _a = "Abubakar";
+var $a = "Abubakar";
+var a = "Abu";
+var b = "bakar";
+var c = a + b;
+var name = "abubakar"; age = 23; country = "Pakistan";
