49cfc3c941e19b7062d81d240544f425f58f0fd9
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-30 12:04:06 +0500

    009-if_statements_nested.js file

---
+++ javascript/book/009-if_statements_nested.js
@@ -0,0 +1,13 @@
+if (10 < 20){
+    document.write("10 is greater than 20"+"<br>");
+    if (10 > 5){
+        document.write("10 is greater than 5"+"<br>");
+    }
+
+    if(5 > 3){
+        document.write("5 is greater than 3"+"<br>");
+        if (3 < 5){
+            document.write("3 is less than 5");
+        }
+    }
+}
