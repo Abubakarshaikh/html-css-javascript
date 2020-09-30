4fb462addf42a91ad3e8fec6875cceab21009a1e
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-30 11:55:16 +0500

    006-comparison_operators.js file

---
+++ javascript/book/006-comparison_operators.js
@@ -0,0 +1,8 @@
+var firstName = "Abubakar";
+var lastName = "Shaikh";
+var fullName = firstName + lastName;
+if (fullName === firstName + lastName){
+    document.write("Your name is abubakar shaikh. ");
+} else {
+    document.write("Not Abubakar");
+}
