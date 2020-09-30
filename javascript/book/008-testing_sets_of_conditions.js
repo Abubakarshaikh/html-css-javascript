a9d36504f13d5a07ca15ab42147e80a68c15b08b
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-30 11:58:28 +0500

    008-testing_sets_of_conditions.js file

---
+++ javascript/book/008-testing_sets_of_conditions.js
@@ -0,0 +1,22 @@
+// var weight = 400;
+// var time = 4;
+// var age = 22;
+// var gender = "male";
+// // && both condition met
+// if (weight > 300 && time < 6 && age < 23 && gender == "male"){
+//     alert("Come to our TryOut!");
+// } else {
+//     alert("Come to our CookOut!");
+// }
+
+
+var SAT = 200;
+var avg = 300;
+var GPA = 3.5;
+var sports = "Football";
+
+if (SAT > avg || GPA > 2.5 || sports == "Football"){
+    alert("welcome to buba state!");
+} else {
+    alert("Have you looked into appliance repair!");
+}
