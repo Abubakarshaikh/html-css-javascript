7debe367809e2a130ac949d7131c4e398da76103
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-30 12:04:37 +0500

    010-arrays.js file

---
+++ javascript/book/010-arrays.js
@@ -0,0 +1,17 @@
+var cleanestCities = ["Calgary", "Zurich", "Luxembourg", "Adeleide", "Singapore"];
+
+var cityToCheck= prompt();
+
+if (cityToCheck == cleanestCities[0]){
+    document.write("it's one of the Cleanest City. ");
+} else if (cityToCheck == cleanestCities[1]){
+    document.write("it's one of the Cleanest City. ");
+} else if (cityToCheck == cleanestCities[2]){
+    document.write("it's one of the Cleanest City. ");
+} else if (cityToCheck == cleanestCities[3]){
+    document.write("it's one of the Cleanest City. ");
+} else if (cityToCheck == cleanestCities[4]){
+    document.write("it's one of the Cleanest City. ");
+} else{
+    document.write("Sorry , it's not clean City. ");
+}
