45dfbaeaea934f9835eb409dbcd3ccd45fa4d17a
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-12-16 12:05:32 +0500

    first assignmemts is Done

---
+++ javascript/assignments/001-add_strings_and_int.dart
@@ -0,0 +1,6 @@
+// Print: Hello, Abubakar Your Age is 23  
+  var firstName = "Abubakar";
+  var LastName = "Shaikh";
+  var age = 23; 
+  
+  console.log("hello"+firstName+"+LastName+" your Age is "+age);
