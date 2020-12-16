8923fd4d95f24a72dd155edcb187ec5128413013
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-12-16 12:15:00 +0500

    finally done

--- javascript/assignments/001-add_strings_and_int.js
+++ javascript/assignments/001-add_strings_and_int.js
@@ -1,6 +1,7 @@
 // Print: Hello, Abubakar Your Age is 23  
-  var firstName = "Abubakar";
-  var LastName = "Shaikh";
-  var age = 23; 
-  
-  console.log("hello"+firstName+"+LastName+" your Age is "+age);
+var firstName = "Abubakar";
+var lastName = "Shaikh";
+
+var age = 23;
+
+console.log("Hello, "+firstName+ " " +lastName+" your age is "+age);
