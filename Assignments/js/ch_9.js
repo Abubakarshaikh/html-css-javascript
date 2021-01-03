56c1dc32b8ee3192b64895f7d7f5596c61cef02c
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2021-01-03 16:27:52 +0500

    assignments chap 09 done

---
+++ Assignments/js/ch_9.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Enter Your First Name");
+var lastName = prompt("Enter Your Last Name");
+var age = prompt("Enter Your Age");
+
+alert("Hello, "+firstName+" "+lastName+" "+"Your are "+age+" "+"Year old");
